686fc23cbf0222a3ff050e8acc74b9d3508a090e
dreysimp <Audrey.Simpson01@student.csulb.edu>
2022-03-19 22:45:53 -0700

    audrey

--- src/main/java/csulb/cecs323/model/Individual_Author.java
+++ src/main/java/csulb/cecs323/model/Individual_Author.java
@@ -1,16 +1,1 @@
-package csulb.cecs323.model;
 
-import javax.persistence.*;
-
-@Entity
-@DiscriminatorValue("IA")
-public class Individual_Author extends Authoring_Entities{
-
-    public Individual_Author(String email, String name){
-        super(email, name);
-    }
-
-    public Individual_Author(){
-        super();
-    }
-}
