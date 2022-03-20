b58ffc01107518f76fda857025e55c5e6923ef36
karypaquot <Karina.Hernandez08@student.csulb.edu>
2022-03-19 21:45:46 -0700

    more updates to authoring entities
    checking discriminating values

--- src/main/java/csulb/cecs323/model/Authoring_Entities.java
+++ src/main/java/csulb/cecs323/model/Authoring_Entities.java
@@ -2,7 +2,9 @@ package csulb.cecs323.model;
 
 import javax.persistence.*;
 
-@Inheritance
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "Authoring_Entity_Type")
 public abstract class Authoring_Entities {
     @Id
     @Column(nullable = false, length = 30)
--- src/main/java/csulb/cecs323/model/Individual_Author.java
+++ src/main/java/csulb/cecs323/model/Individual_Author.java
@@ -1,4 +1,9 @@
 package csulb.cecs323.model;
 
+import javax.persistence.DiscriminatorValue;
+import javax.persistence.Entity;
+
+@Entity
+@DiscriminatorValue("IA")
 public class Individual_Author {
 }
