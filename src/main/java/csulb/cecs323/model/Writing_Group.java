6e77f52762e4530979aa3f7d20277b0afe09b4db
dreysimp <Audrey.Simpson01@student.csulb.edu>
2022-03-19 21:12:25 -0700

    Update Writing_Group.java

--- src/main/java/csulb/cecs323/model/Writing_Group.java
+++ src/main/java/csulb/cecs323/model/Writing_Group.java
@@ -2,11 +2,18 @@ package csulb.cecs323.model;
 import javax.persistence.*;
 
 @Entity
-public class Writing_Group {
+public class Writing_Group extends Authoring_Entities{
     @Id
     @Column (nullable = false, length = 80)
     private String head_writer;
 
     @Column(nullable = false)
     private int year_formed;
+
+    public Writing_Group(){}
+
+    public Writing_Group(String head_writer, int year_formed){
+        this.head_writer = head_writer;
+        this.year_formed = year_formed;
+    }
 }
