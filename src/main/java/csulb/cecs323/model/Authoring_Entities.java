d710d20379246f529d56bca43a746e272a57ea03
karypaquot <Karina.Hernandez08@student.csulb.edu>
2022-03-19 23:43:32 -0700

    Update Authoring_Entities.java

--- src/main/java/csulb/cecs323/model/Authoring_Entities.java
+++ src/main/java/csulb/cecs323/model/Authoring_Entities.java
@@ -1,1 +1,29 @@
+package csulb.cecs323.model;
+
+import javax.persistence.*;
+
+@Entity
+@Inheritance(strategy = InheritanceType.SINGLE_TABLE)
+@DiscriminatorColumn(name = "Authoring_Entity_Type", discriminatorType = DiscriminatorType.STRING, length = 50)
+public abstract class Authoring_Entities {
+    @Id
+    @Column(nullable = false, length = 30)
+    private String email;
+
+
+    @Column (nullable = false, length = 80)
+    private String name;
+
+    public Authoring_Entities (String email, String name){
+        this.email = email;
+        this.name = name;
+    }
+    public Authoring_Entities() {}
+
+    @Override
+    public String toString () {
+        return "Authoring name:  " + name + " Email: " + email;
+    }
+
+}
 
