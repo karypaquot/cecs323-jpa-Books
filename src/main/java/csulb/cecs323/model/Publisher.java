efbf1f2f6d4fce841a436afb62cedda20ae98f9b
Steven Nguyen <80537958+daxile6@users.noreply.github.com>
2022-03-19 21:07:24 -0700

    Update Publisher.java

--- src/main/java/csulb/cecs323/model/Publisher.java
+++ src/main/java/csulb/cecs323/model/Publisher.java
@@ -6,6 +6,9 @@ import javax.persistence.*;
 @Entity
 public class Publisher {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private long ID;
+
     @Column(nullable = false, length = 80)
     private String name;
 
@@ -15,20 +18,55 @@ public class Publisher {
     @Column (nullable = false, length = 24)
     private String email;
 
-    @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(nullable = false)
-    private Books books;
+//    @ManyToOne(fetch = FetchType.LAZY)
+//    @JoinColumn(nullable = false)
+//    private Books books;
 
-    public Publisher (Books book, String name, String phone, String email){
-        this.books = book;
+    public Publisher (String name, String phone, String email){
         this.name = name;
         this.phone = phone;
         this.email = email;
     }
     public Publisher(){}
-    
-    public String toString(){
-        return "\n\nPublisher: " + name + "\nPhone: " + phone + "\nEmail: " + email;
+
+
+    public long get_publisherID() {
+
+        return ID;
     }
+
+    public String getName() {
+
+        return name;
+    }
+
+    public void setName(String Name) {
+
+        this.name = name;
+    }
+
+    public String getPhone(){
+        return phone;
+    }
+    public void setPhone(String phone){
+        this.phone = phone;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setEmail(){
+        this.email = email;
+    }
+    @Override
+    public String toString () {
+        return "Publisher - ID: " + this.get_publisherID() + " Name: " + this.getName() +
+                " Phone #: " + this.getPhone() + "Email " + this.getEmail();
+    }
+
+
+
+
 
 }
