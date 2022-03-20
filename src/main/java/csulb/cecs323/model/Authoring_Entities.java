c109541c7ed523c2d068b790528bbe9a6964ae63
karypaquot <Karina.Hernandez08@student.csulb.edu>
2022-03-19 21:10:51 -0700

    update authoring entities

--- src/main/java/csulb/cecs323/model/Authoring_Entities.java
+++ src/main/java/csulb/cecs323/model/Authoring_Entities.java
@@ -2,8 +2,8 @@ package csulb.cecs323.model;
 
 import javax.persistence.*;
 
-@Entity
-public class Authoring_Entities {
+@MappedSuperclass
+public abstract class Authoring_Entities {
     @Id
     @Column(nullable = false, length = 30)
     private String email;
@@ -20,5 +20,10 @@ public class Authoring_Entities {
     @Column(nullable = false)
     private int year_formed;
     */
+    public Authoring_Entities (String email, String name){
+        this.email = email;
+        this.name = name;
+    }
+    
 
 }
