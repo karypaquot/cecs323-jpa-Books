f20a0840790b8fddb68dab7155b62004e71039fa
Steven Nguyen <80537958+daxile6@users.noreply.github.com>
2022-03-19 22:36:38 -0700

    Update Ad_Hoc_Teams_Member.java

--- src/main/java/csulb/cecs323/model/Ad_Hoc_Teams_Member.java
+++ src/main/java/csulb/cecs323/model/Ad_Hoc_Teams_Member.java
@@ -7,4 +7,11 @@ import javax.persistence.Entity;
 @DiscriminatorValue("AHT")
 public class Ad_Hoc_Teams_Member extends Authoring_Entities{
 
+    public Ad_Hoc_Teams_Member(String email, String name){
+        super(email,name);
+    }
+    public Ad_Hoc_Teams_Member(){
+        super();
+    }
+
 }
