0c11a1154a1f08de5f817329923d2a1fda1bae11
karypaquot <Karina.Hernandez08@student.csulb.edu>
2022-03-19 20:03:04 -0700

    changes to authoring entities and writing group class
    Took out head_write and year_formed annotations

--- src/main/java/csulb/cecs323/model/Writing_Group.java
+++ src/main/java/csulb/cecs323/model/Writing_Group.java
@@ -1,4 +1,12 @@
 package csulb.cecs323.model;
+import javax.persistence.*;
 
+@Entity
 public class Writing_Group {
+    @Id
+    @Column (nullable = false, length = 80)
+    private String head_writer;
+
+    @Column(nullable = false)
+    private int year_formed;
 }
