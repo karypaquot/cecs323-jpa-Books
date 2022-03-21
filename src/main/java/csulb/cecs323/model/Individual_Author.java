72ca38a1f07ef1ac23ea7695fd722b9efd6ab1bd
Steven Nguyen <80537958+daxile6@users.noreply.github.com>
2022-03-20 20:13:47 -0700

    Update Individual_Author.java

--- src/main/java/csulb/cecs323/model/Individual_Author.java
+++ src/main/java/csulb/cecs323/model/Individual_Author.java
@@ -1,1 +1,19 @@
+package csulb.cecs323.model;
+
+import javax.persistence.*;
+@Entity
+@DiscriminatorValue("Individual_Author")
+public class Individual_Author extends Authoring_Entities{
+    public Individual_Author(String name, String email){
+        super(name,email);
+    }
+    public Individual_Author(){
+    }
+
+    @Override
+    public String toString(){
+        return super.toString() + "\nIndividual Writer: " + getName() + "\nIndividual Writer's Email " + this.getEmail();
+    }
+
+}
 
