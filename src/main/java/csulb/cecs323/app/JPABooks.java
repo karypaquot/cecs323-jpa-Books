7b6527ca4d5801c64c6b892093c7e93a5e9dfff7
Steven Nguyen <80537958+daxile6@users.noreply.github.com>
2022-03-20 21:33:21 -0700

    hi

--- src/main/java/csulb/cecs323/app/JPABooks.java
+++ src/main/java/csulb/cecs323/app/JPABooks.java
@@ -81,7 +81,7 @@ public class JPABooks {
       EntityTransaction tx = manager.getTransaction();
 
       tx.begin();
-
+   //hi
       //create publisher list and prompt user to enter information
       List <Publishers> publishers = new ArrayList<Publishers>();
       Publishers publisher = createPublishers(publishers);
