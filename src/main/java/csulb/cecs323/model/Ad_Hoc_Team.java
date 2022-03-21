3f7e2fef26427011263ae1b5d22dfebfc0c9f90b
dreysimp <Audrey.Simpson01@student.csulb.edu>
2022-03-20 20:44:16 -0700

    manytomany

--- src/main/java/csulb/cecs323/model/Ad_Hoc_Team.java
+++ src/main/java/csulb/cecs323/model/Ad_Hoc_Team.java
@@ -1,10 +1,19 @@
 package csulb.cecs323.model;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @DiscriminatorValue("Ad Hoc Team")
 public class Ad_Hoc_Team extends Authoring_Entities{
+
+    @ManyToMany
+    @JoinTable (
+            name = "individual_on_team",
+            joinColumns = @JoinColumn(name = "team_name"),
+            inverseJoinColumns = @JoinColumn(name = "individual_name")
+    )
+    Set<Individual_Author> individual_authors;
 
     public Ad_Hoc_Team(){}
 
--- src/main/java/csulb/cecs323/model/Individual_Author.java
+++ src/main/java/csulb/cecs323/model/Individual_Author.java
@@ -1,9 +1,15 @@
 package csulb.cecs323.model;
 
 import javax.persistence.*;
+import java.util.Set;
+
 @Entity
 @DiscriminatorValue("Individual_Author")
 public class Individual_Author extends Authoring_Entities{
+
+    @ManyToMany (mappedBy = "individual_authors")
+    Set<Ad_Hoc_Team> ad_hoc_teams;
+
     public Individual_Author(String name, String email){
         super(name,email);
     }
